3804be93a2bed16dcd9e95384c53f1db3d40a93d
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2026-02-01 17:09:48 +0100

    init

---
+++ src/main/java/com/peselgenerator/PeselgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.peselgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeselgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeselgeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peselgenerator/PeselgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peselgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeselgeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
