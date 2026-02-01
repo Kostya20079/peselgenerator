2ab65d0a3f26ac3fa64fadd1bf0d9dd2ea840b77
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2026-02-01 19:18:57 +0100

    add auth controller and register.html page

---
+++ src/main/java/com/peselgenerator/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.peselgenerator.controller;
+
+import com.peselgenerator.dto.RegisterRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequest());
+        return "register";
+    }
+}
